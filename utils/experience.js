ea0a4ee05d014ab599ab4ef833f7e8b2ece3bcc9
Harsh <hrsri.x@gmail.com>
2024-11-28 15:23:43 +0530

    level up canvas test 6

--- utils/experience.js
+++ utils/experience.js
@@ -3,36 +3,6 @@ import {
   updateUser
 } from '../database.js';
 
-/* export async function updateExpPoints(content, user, channel) {
-  try {
-    const userData = await getUserData(user.id);
-    if (!userData) return
-    userData.exp += 10;
-    if (content.includes("kas")) userData.exp += 10;
-
-    let threshold = 100;
-    let lvl = Math.floor(Math.sqrt(userData.exp / threshold)) || 0;
-    let lvlUpReward = 10;
-    let lvlUp = false;
-
-    if (!(lvl === userData.level)) {
-      userData.level = lvl;
-      lvlUpReward = 1500 + userData.level * 1000;
-      userData.cash += lvlUpReward;
-      lvlUp = true;
-    }
-
-    await updateUser(user.id, userData);
-
-    if (lvlUp) {
-      return channel.send(`🎉 **${user.username}** has leveled up! 🎉\n\n💰 You've received <:kasiko_coin:1300141236841086977> **${lvlUpReward}** 𝑪𝒂𝒔𝒉 as a reward!\n🏆 **New Level:** ${lvl}`);
-    }
-    return;
-  } catch (e) {
-    console.error(e);
-  }
-}
-*/
 
 import {
   AttachmentBuilder
@@ -42,6 +12,12 @@ import {
   createCanvas,
   loadImage
 } from '@napi-rs/canvas';
+
+const crown = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M19.3486 5.17917L16.1111 8.31806L12.9147 3.93556C12.8285 3.7672 12.6974 3.62592 12.536 3.52726C12.3746 3.42861 12.1892 3.3764 12 3.3764C11.8108 3.3764 11.6254 3.42861 11.464 3.52726C11.3026 3.62592 11.1715 3.7672 11.0853 3.93556L7.88889 8.31806L4.65139 5.17917C4.53548 4.99163 4.36233 4.84633 4.15752 4.76473C3.95271 4.68313 3.72709 4.66955 3.51397 4.726C3.30085 4.78245 3.11153 4.90593 2.97397 5.07821C2.8364 5.2505 2.75788 5.46245 2.75 5.68278V15.4847C2.75 16.8476 3.29142 18.1547 4.25515 19.1185C5.21887 20.0822 6.52597 20.6236 7.88889 20.6236H16.1111C17.474 20.6236 18.7811 20.0822 19.7449 19.1185C20.7086 18.1547 21.25 16.8476 21.25 15.4847V5.68278C21.2421 5.46245 21.1636 5.2505 21.026 5.07821C20.8885 4.90593 20.6991 4.78245 20.486 4.726C20.2729 4.66955 20.0473 4.68313 19.8425 4.76473C19.6377 4.84633 19.4645 4.99163 19.3486 5.17917Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8 16.5125H16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`
 
 async function generateLevelUpImage(user, lvlUpReward, lvl, expRequiredNextLvl, pfp) {
   const padding = 20;
@@ -65,7 +41,7 @@ async function generateLevelUpImage(user, lvlUpReward, lvl, expRequiredNextLvl, 
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
   ctx.font = 'bold 55px Tahoma';
-  ctx.fillText(`✯⁠ New Level ${lvl} ✯⁠`, canvas.width / 1.6, padding + 100);
+  ctx.fillText(`⁠ New Level ${lvl}⁠`, canvas.width / 1.6, padding + 100);
   ctx.font = 'italic 34px Tahoma';
   ctx.fillText(`✷⁠ You've received $${lvlUpReward} cash!`, canvas.width / 2.48, padding + 240);
   ctx.font = 'italic 27px Tahoma';
@@ -79,13 +55,16 @@ async function generateLevelUpImage(user, lvlUpReward, lvl, expRequiredNextLvl, 
   ctx.stroke();
 
   try {
+    const svgImage = await loadImage('data:image/svg+xml;base64,' + Buffer.from(crown).toString('base64'));
+    ctx.drawImage(svgImage, canvas.width / 1.45, padding + 100, 50, 50);
+
     const profileImage = await loadImage(pfp);
     const imageSize = 130;
     ctx.drawImage(profileImage, padding + 40, padding + 40, imageSize, imageSize);
     const buffer = canvas.toBuffer('image/png');
 
     const attachment = new AttachmentBuilder(buffer, {
-      name: 'kas-level-up-canvas-image.png'
+      name: 'kasi-level-up-canvas-image.png'
     });
 
     return attachment; // This will now return the attachment correctly
