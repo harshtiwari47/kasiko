86fb11da2943089506916d82769a1a90a4513c73
Harsh <hrsri.x@gmail.com>
2024-12-06 16:06:12 +0530

    fix & redesign bj

--- src/txtcommands/games/blackjack.js
+++ src/txtcommands/games/blackjack.js
@@ -13,21 +13,21 @@ import {
 } from '../../../helper.js';
 
 const cardDeck = [
-  "<:club2:1314220338430738443>", "<:club3:1314220364829560843>", "<:club4:1314220419091533884>", "<:club5:1314220442730500116>", "<:club6:1314220469578235904>", "<:club7:1314220500058378240>", "<:club8:1314220524213243984>", "<:club9:1314220548028633179>", "<:club10:1314220568865935423>", "<:heartJ:1314220885942734888>", "<:heartQ:1314221003076800514>", "<:heartK:1314220904665845761>", "<:heartA:1314220849317941380>"
+  "<:club2:1314435229531770990>", "<:club3:1314435207482572820>", "<:club4:1314435184590065775>", "<:club5:1314435159298412554>", "<:club6:1314435137005420586>", "<:club7:1314435116466044928>", "<:club8:1314435090838982678>", "<:club9:1314435070496342066>", "<:club10:1314435046450659328>", "<:heartJ:1314435267205009458>", "<:heartQ:1314435305666908181>", "<:heartK:1314435286847062057>", "<:heartA:1314435334641156238>"
 ];
 
 function getCardValue(card) {
-  if (card === "<:heartJ:1314220885942734888>" || card === "<:heartQ:1314221003076800514>" || card === "<:heartK:1314220904665845761>") return 10;
-  if (card === "<:heartA:1314220849317941380>") return 11;
-  if (card === "<:club2:1314220338430738443>") return 2;
-  if (card === "<:club3:1314220364829560843>") return 3;
-  if (card === "<:club4:1314220419091533884>") return 4;
-  if (card === "<:club5:1314220442730500116>") return 5;
-  if (card === "<:club6:1314220469578235904>") return 6;
-  if (card === "<:club7:1314220500058378240>") return 7;
-  if (card === "<:club8:1314220524213243984>") return 8;
-  if (card === "<:club9:1314220548028633179>") return 9;
-  if (card === "<:club10:1314220568865935423>") return 10;
+  if (card === "<:heartJ:1314435267205009458>" || card === "<:heartQ:1314435305666908181>" || card === "<:heartK:1314435286847062057>") return 10;
+  if (card === "<:heartA:1314435334641156238>") return 11;
+  if (card === "<:club2:1314435229531770990>") return 2;
+  if (card === "<:club3:1314435207482572820>") return 3;
+  if (card === "<:club4:1314435184590065775>") return 4;
+  if (card === "<:club5:1314435159298412554>") return 5;
+  if (card === "<:club6:1314435137005420586>") return 6;
+  if (card === "<:club7:1314435116466044928>") return 7;
+  if (card === "<:club8:1314435090838982678>") return 8;
+  if (card === "<:club9:1314435070496342066>") return 9;
+  if (card === "<:club10:1314435046450659328>") return 10;
 }
 
 function calculateHandValue(hand) {
@@ -37,7 +37,7 @@ function calculateHandValue(hand) {
   hand.forEach(card => {
     const value = getCardValue(card);
     totalValue += value;
-    if (card === "<:heartA:1314220849317941380>") aceCount++;
+    if (card === "<:heartA:1314435334641156238>") aceCount++;
   });
 
   while (totalValue > 21 && aceCount > 0) {
@@ -77,13 +77,23 @@ export async function blackjack(id, amount, channel) {
     const playerHandValue = calculateHandValue(playerHand);
     const botHandValue = calculateHandValue([botHand[0], "X"]);
 
+    const avatarUrl = guild.user.displayAvatarURL({
+      dynamic: true, size: 1024
+    });
+
+    const embedTitle = new EmbedBuilder()
+    .setTitle('𝗕𝗹𝗮𝗰𝗸𝗷𝗮𝗰𝗸 𝗚𝗮𝗺𝗲 🃏')
+    .setAuthor({
+      name: `${guild.user.username}'s`, iconURL: avatarUrl
+    })
+    .setThumbnail(`https://harshtiwari47.github.io/kasiko-public/images/blackjack-icon.png`)
+
     // Create embed for the game state
     const embed = new EmbedBuilder()
-    .setColor('#0099ff')
-    .setTitle('🃏 𝐁𝐥𝐚𝐜𝐤𝐣𝐚𝐜𝐤 𝐆𝐚𝐦𝐞')
-    .setDescription(`**${guild.user.username}**, you are playing Blackjack!\n\n` +
-      `**Your cards**: ${playerHand.join(" ")} (**${playerHandValue}**)\n` +
-      `**Bot's cards**: ${botHand[0]} X (**???**)\n\n` +
+    .setColor('#8d69f2')
+    .setDescription(`> **${guild.user.username}**, you are playing Blackjack!\n\n` +
+      `**Your cards** :\n${playerHand.join(" ")} (**${playerHandValue}**)\n` +
+      `**Bot's cards**:\n${botHand[0]} <:unknownCard:1314464932472946768> (**?**)\n\n` +
       `Bet: <:kasiko_coin:1300141236841086977> **${amount.toLocaleString()}**`);
 
     // Create buttons for hit and stand
@@ -100,13 +110,13 @@ export async function blackjack(id, amount, channel) {
 
     // Send the embed with buttons
     const gameMessage = await channel.send({
-      embeds: [embed], components: [row]
+      embeds: [embedTitle, embed], components: [row]
     });
 
     // Button interaction handler
     const filter = (interaction) => interaction.user.id === id;
     const collector = gameMessage.createMessageComponentCollector({
-      filter, time: 30000
+      filter, time: 40000
     });
 
     collector.on('collect', async (interaction) => {
@@ -117,26 +127,26 @@ export async function blackjack(id, amount, channel) {
         if (newPlayerValue > 21) {
           const bustEmbed = new EmbedBuilder(embed)
           .setColor("#f43d3d")
-          .setDescription(`🚨 **${guild.user.username}**, you busted! Your hand value is over 21.\n` +
-            `**Your cards :** ${playerHand.join(" ")} (**${newPlayerValue}**)\n` +
-            `**Bot's cards:** ${botHand[0]} X (**???**)\n\n` +
+          .setDescription(`> 🚨 **${guild.user.username}**, you busted! Your hand value is over 21.\n\n` +
+            `**Your cards :**\n${playerHand.join(" ")} (**${newPlayerValue}**)\n` +
+            `**Bot's cards:**\n${botHand[0]} <:unknownCard:1314464932472946768> (**?**)\n\n` +
             `Bet: <:kasiko_coin:1300141236841086977> **${amount.toLocaleString()}**`);
 
           await interaction.update({
-            embeds: [bustEmbed], components: []
+            embeds: [embedTitle, bustEmbed], components: []
           });
-
+          collector.stop("busted")
           return;
         } else {
           // Update the embed with new player hand
           const newEmbed = new EmbedBuilder(embed)
-          .setDescription(`**${guild.user.username}**, you hit!\n\n` +
-            `**Your cards :** ${playerHand.join(" ")} (**${newPlayerValue}**)\n` +
-            `**Bot's cards:** ${botHand[0]} X (**???**)\n\n` +
+          .setDescription(`> **${guild.user.username}**, you hit!\n\n` +
+            `**Your cards :**\n${playerHand.join(" ")} (**${newPlayerValue}**)\n` +
+            `**Bot's cards:**\n${botHand[0]} <:unknownCard:1314464932472946768> (**?**)\n\n` +
             `Bet: <:kasiko_coin:1300141236841086977> **${amount.toLocaleString()}**`);
 
           await interaction.update({
-            embeds: [newEmbed], components: [row]
+            embeds: [embedTitle, newEmbed], components: [row]
           });
         }
       } else if (interaction.customId === 'stand') {
@@ -174,24 +184,26 @@ export async function blackjack(id, amount, channel) {
         // Final update to the embed
         const finalEmbed = new EmbedBuilder(embed)
         .setColor(color)
-        .setDescription(`${resultMessage}\n\n` +
-          `**Your cards :** ${playerHand.join(" ")} (**${finalPlayerHandValue}**)\n` +
-          `**Bot's cards:** ${botHand.join(" ")} (**${botHandFinalValue}**)\n\n` +
+        .setDescription(`> ${resultMessage}\n\n` +
+          `**Your cards :**\n${playerHand.join(" ")} (**${finalPlayerHandValue}**)\n` +
+          `**Bot's cards:**\n${botHand.join(" ")} (**${botHandFinalValue}**)\n\n` +
           `Bet: <:kasiko_coin:1300141236841086977> **${amount.toLocaleString()}**`);
 
         await interaction.update({
-          embeds: [finalEmbed], components: []
+          embeds: [embedTitle, finalEmbed], components: []
         });
+
+        collector.stop("result")
         return;
       }
     });
 
     collector.on('end',
-      () => {
+      (collected, reason) => {
         // Handle timeout if the game ends with no interaction
-        if (!gameMessage.deleted) {
+        if (!gameMessage.deleted && reason === "time") {
           gameMessage.edit({
-            embeds: [embed.setFooter({
+            embeds: [embedTitle, embed.setFooter({
               text: "⏱️ Timeout"
             }).setColor("#f43d3d")],
             components: []
--- src/txtcommands/games/horserace.js
+++ src/txtcommands/games/horserace.js
@@ -10,7 +10,7 @@ import {
   EmbedBuilder
 } from 'discord.js';
 
-export async function horseRace(id, amount, channel, betOn = "horse1", teammateId = null) {
+export async function horseRace(id, amount, channel, betOn = "horse1", opponentBetOn = "horse2", teammateId = null) {
   try {
     const guild = await channel.guild.members.fetch(id);
     let userData = await getUserData(id);
@@ -32,39 +32,56 @@ export async function horseRace(id, amount, channel, betOn = "horse1", teammateI
 
     if (teammateData) {
       gameMessage = await channel.send(
-        `🏇 **${guild.user.username}** has started a horse race and bet <:kasiko_coin:1300141236841086977> **${amount.toLocaleString()}**! Type **participate** to join the race. **<@${teammateId}>**, you have 25 seconds!`
+        `🏇 **${guild.user.username}** has started a horse race and bet <:kasiko_coin:1300141236841086977> **${amount.toLocaleString()}**! Type **participate <horse (optional)>** (horse1, horse2, horse3) to join the race.**<@${teammateId}>**, you have 25 seconds!`
       );
 
-      const filter = (m) => m.content.toLowerCase() === "participate" && m.author.id === teammateId;
+      const filter = (m) => m.content.toLowerCase().startsWith() === "participate" && m.author.id === teammateId;
       const collector = channel.createMessageCollector({
         filter, time: 25000
       });
 
       collector.on("collect", async (msg) => {
         const opponent = msg.author;
-        collector.stop("opponent_joined");
-        channel.send(`🎉 **${opponent.username}** has joined the race! Let the race begin!`);
 
-        // Call the function to start the game here once the opponent joins
-        await startRace(amount, betOn, teammateId, userData, teammateData, channel, guild);
-      });
+        const msgArgs = msg.slice(prefix.toLowerCase().length).trim().split(/ +/);
 
-      collector.on("end", async (collected, reason) => {
-
-        userData.cash += amount;
-        if (teammateData) teammateData.cash += amount;
-
-        // Save the updated cash to the database
-        await updateUser(id, userData);
-        if (teammateData) await updateUser(teammateId, teammateData);
-
-        if (reason !== "opponent_joined") {
-          return channel.send(`⏱️ No one joined the race. The game has been canceled, and your bet has been refunded.`);
+        if (msgArgs && msgArgs[1] && (msgArgs[1] === "horse1" || msgArgs[1] === "horse2" || msgArgs[1] === "horse3")) {
+          if (msgArgs === betOn) {
+            if (betOn === "horse1") opponentBetOn = "horse2"
+            if (betOn === "horse2") opponentBetOn = "horse3"
+            if (betOn === "horse3") opponentBetOn = "horse1"
+          } else {
+            opponentBetOn = msgArgs[1];
+          }
+        } else {
+          if (betOn === "horse1") opponentBetOn = "horse2"
+          if (betOn === "horse2") opponentBetOn = "horse3"
+          if (betOn === "horse3") opponentBetOn = "horse1"
         }
+
+        collector.stop("opponent_joined");
+        channel.send(`🎉 **${opponent.username}** has joined the race with bet on **${opponentBetOn}**! Let the race begin!`);
+        // Call the function to start the game here once the opponent joins
+        await startRace(amount, betOn, opponentBetOn, teammateId, userData, teammateData, channel, guild);
       });
+
+      collector.on("end",
+        async (collected, reason) => {
+
+          userData.cash += amount;
+          if (teammateData) teammateData.cash += amount;
+
+          // Save the updated cash to the database
+          await updateUser(id, userData);
+          if (teammateData) await updateUser(teammateId, teammateData);
+
+          if (reason !== "opponent_joined") {
+            return channel.send(`⏱️ No one joined the race. The game has been canceled, and your bet has been refunded.`);
+          }
+        });
     } else {
       // If no teammate, skip the message collection and start race directly
-      await startRace(amount, betOn, teammateId, userData, teammateData, channel, guild);
+      await startRace(amount, betOn, opponentBetOn, teammateId, userData, teammateData, channel, guild);
     }
     return;
   } catch (e) {
@@ -74,7 +91,7 @@ export async function horseRace(id, amount, channel, betOn = "horse1", teammateI
 }
 
 // Function to start the race logic
-async function startRace(amount, betOn, teammateId, userData, teammateData, channel, guild) {
+async function startRace(amount, betOn, opponentBetOn, teammateId, userData, teammateData, channel, guild) {
   amount = parseInt(amount)
   const suspenseMessage = await channel.send(
     `🏁 The race is about to begin!\n`
@@ -83,29 +100,42 @@ async function startRace(amount, betOn, teammateId, userData, teammateData, chan
   // Race details
   const horse1 = "<:horse_brown:1314077268447985725>";
   const horse2 = "<:horse_red:1314077243881820241>";
+  const horse3 = "<:horse_red:1314077243881820241>";
   const trackLength = 20; // Length of the race track
   let horse1Pos = 0;
   let horse2Pos = 0;
+  let horse3Pos = 0;
 
   const raceInterval = setInterval(async () => {
     horse1Pos += Math.floor(Math.random() * 8); // Random step for horse 1
     horse2Pos += Math.floor(Math.random() * 8); // Random step for horse 2
+    horse3Pos += Math.floor(Math.random() * 8); // Random step for horse 2
 
     // Update the track
     let track1 = `${' '.repeat(horse1Pos)}${horse1}${' '.repeat(Math.max(0, trackLength - horse1Pos))}|`;
     let track2 = `${' '.repeat(horse2Pos)}${horse2}${' '.repeat(Math.max(0, trackLength - horse2Pos))}|`;
+    let track3 = `${' '.repeat(horse2Pos)}${horse2}${' '.repeat(Math.max(0, trackLength - horse2Pos))}|`;
 
-    await suspenseMessage.edit(`🏁 𝑻𝒉𝒆 𝒓𝒂𝒄𝒆 𝒊𝒔 𝒐𝒏!\n\n${track1}\n${track2}\n` + `**${guild.user.username}** bet on ${betOn === "horse1" ? "<:horse_brown:1314077268447985725> Horse 1": "<:horse_red:1314077243881820241> Horse 2"} for <:kasiko_coin:1300141236841086977> **${amount.toLocaleString()}** cash!` +
-      `${teammateId ? `\n**<@${teammateId}>** is teaming up with the same bet!`: ""}`
+    await suspenseMessage.edit(`🏁 𝑻𝒉𝒆 𝒓𝒂𝒄𝒆 𝒊𝒔 𝒐𝒏!\n\n${track1}\n${track2}\n${track3}\n` + `**${guild.user.username}** bet on **${betOn === "horse1" ? horse1 + " Horse 1": betOn === "horse2" ? horse2 + " Horse 2 ": horse3 + " Horse 3"}** for <:kasiko_coin:1300141236841086977> **${amount.toLocaleString()}** cash!` +
+      `${teammateId ? `\n**<@${teammateId}>** is teaming up with the same bet on **${opponentBetOn}**!`: ""}`
     );
 
     // Check if any horse has finished
-    if (horse1Pos >= trackLength || horse2Pos >= trackLength) {
+    if (horse1Pos >= trackLength || horse2Pos >= trackLength || horse3Pos >= trackLength) {
       clearInterval(raceInterval);
 
       // Determine the winner
-      const winner = (horse1Pos >= trackLength && horse1Pos > horse2Pos) ? "horse1": "horse2";
-      const winAmount = Math.floor(amount * 1);
+      let winner = (horse1Pos >= trackLength && horse1Pos > horse2Pos) ? "horse1": "horse2";
+      if (winner === "horse1") {
+        if (horse3Pos >= trackLength && horse3Pos > horse1Pos) {
+          winner = "horse3";
+        }
+      } else {
+        if (horse3Pos >= trackLength && horse3Pos > horse2Pos) {
+          winner = "horse3";
+        }
+      }
+      const winAmount = Math.floor(amount * 2);
       const teammateSplit = teammateId ? Math.floor(winAmount / 2): winAmount;
 
       if (winner === betOn) {
@@ -116,7 +146,7 @@ async function startRace(amount, betOn, teammateId, userData, teammateData, chan
         .setColor(0xFFD700) // Gold color
         .setTitle(`🎉 ${guild.user.username}, your bet paid off!`)
         .setDescription(
-          `Your bet on **${winner === "horse1" ? "<:horse_brown:1314077268447985725> Horse 1": "<:horse_red:1314077243881820241> Horse 2"}** paid off!\n` +
+          `Your bet on **${winner === "horse1" ? "<:horse_brown:1314077268447985725> Horse 1": winner === "horse2" ? "<:horse_red:1314077243881820241> Horse 2 ": horse3 + " Horse 3"}** paid off!\n` +
           `**${winner === "horse1" ? horse1: horse2}** won the race! 🏆\n` +
           `You earned <:kasiko_coin:1300141236841086977> **${winAmount.toLocaleString()}** cash${teammateId ? `, split with your teammate.`: '.'}`
         )
@@ -128,17 +158,18 @@ async function startRace(amount, betOn, teammateId, userData, teammateData, chan
           embeds: [embed]
         });
       } else {
-        if (teammateData) {
+        if (teammateData && winner === opponentBetOn) {
           teammateData.cash += amount + winAmount;
           await updateUser(teammateId, teammateData);
         }
 
         const embed = new EmbedBuilder()
         .setColor(0xFF0000) // Red color for loss
-        .setTitle(`🚨 ${guild.user.username}, better luck next time!`)
+        .setTitle(`🚨 ${guild.user.username}${teammateData && winner !== opponentBetOn ? " & <@" + teammateId + ">" : ""}, better luck next time!`)
         .setDescription(
-          `Your bet on **${betOn === "horse1" ? "<:horse_brown:1314077268447985725> Horse 1": "<:horse_red:1314077243881820241> Horse 2"}** didn't win...\n` +
-          `**${winner === "horse1" ? horse1: horse2}** crossed the finish line first! Better luck next time!`
+          `**${guild.user.username}** bet on **${betOn === "horse1" ? horse1 + " Horse 1": betOn === "horse2" ? horse2 + " Horse 2 ": horse3 + " Horse 3"}** didn't win...\n` +
+          `${teammateId && winner !== opponentBetOn ? `**<@${teammateId}>** bet on **${opponentBetOn}** didn't win too!`: ""}` +
+          `${teammateId && winner === opponentBetOn ? `**<@${teammateId}>**'s `: ""}**${winner === "horse1" ? horse1 + " Horse 1": winner === "horse2" ? horse2 + " Horse 2 ": horse3 + " Horse 3"}** crossed the finish line first! Better luck next time!`
         )
         .setFooter({
           text: 'Keep trying, your big win is just around the corner!'
@@ -165,7 +196,8 @@ export default {
   async execute(args,
     message) {
     const amount = parseInt(args[1]);
-    const betOn = args[2]?.toLowerCase() === "horse2" ? "horse2": "horse1";
+    let betOn = args[2]?.toLowerCase() === "horse2" ? "horse2": "horse1";
+    betOn = args[2]?.toLowerCase() === "horse3" ? "horse3": betOn;
     const teammateMention = message.mentions.users.first();
     const teammateId = teammateMention ? teammateMention.id: null;
 
