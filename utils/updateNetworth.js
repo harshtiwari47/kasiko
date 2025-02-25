ddad3f51fa187987b50d5fcb4fdfba1e464752f7
Harsh <hrsri.x@gmail.com>
2025-02-25 09:08:45 +0530

    object fixed

--- utils/updateNetworth.js
+++ utils/updateNetworth.js
@@ -32,7 +32,7 @@ export async function calculateNetWorth(userData) {
 
   // Calculate stock values.
   if (userData.stocks && typeof userData.stocks === "object") {
-    for (const stockName in userData.stocks.toJSON()) {
+    for (const stockName in userData.stocks) {
       if (stockName === "_id") continue;
       if (userData.stocks[stockName] && stockData[stockName] && stockData[stockName].currentPrice) {
         const numShares = userData.stocks[stockName].shares;
