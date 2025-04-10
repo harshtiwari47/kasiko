42724ff006d687bbf159bd3496039b426e224acb
Harsh <hrsri.x@gmail.com>
2025-04-10 10:45:04 +0530

    new ship

--- src/txtcommands/fun/ship.js
+++ src/txtcommands/fun/ship.js
@@ -171,7 +171,9 @@ const ShipCmd = {
 
       ctx.fillStyle = "rgb(196,0,0)";
       ctx.font = "30px sans-serif";
-      ctx.fillText(`${score}%`, circleX - 60, circleY + 120);
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(`${score}%`, circleX - 120, circleY + 120);
 
       const attachment = new AttachmentBuilder(await canvas.encode("png"), {
         name: "ship.png"
